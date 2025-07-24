9f129458ab59f6d9c1ac07d201a37e3fe5af81f0
Shinde Aditya <heyshinde@icloud.com>
2025-07-24 16:52:30 +0530

    Initial Commit

---
+++ ClipStructTests/ClipStructTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipStructTests.swift
+//  ClipStructTests
+//
+//  Created by Shinde on 24/07/25.
+//
+
+import Testing
+@testable import ClipStruct
+
+struct ClipStructTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
